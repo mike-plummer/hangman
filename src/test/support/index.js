901654e3a4c43f869d399bc434081b510bc48649
Mike Plummer <mike-plummer@users.noreply.github.com>
2018-08-04 09:39:34 -0500

    Ignore script errors

--- src/test/support/index.js
+++ src/test/support/index.js
@@ -1,1 +1,7 @@
 import './commands';
+
+// CUSTOMIZATION
+// We don't want to fail our tests because of an uncaught script error on the site
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false
+});
